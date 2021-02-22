4d1d63cec3da296ec83caf533575c36a62751a9a
Simon Scatton <simon.scatton@epita.fr>
2021-02-22 12:48:08 +0100

    Changed some comments, removed the main import

--- src/datatypes/linked_list.c
+++ src/datatypes/linked_list.c
@@ -3,7 +3,6 @@
 #include "datatypes/linked_list.h"
 
 /**
- * @brief Creates a linked list sentinel and allocates memory.
  * Allocates memory for a new sentinel and sets the data
  * to SENTINAL_DATA defined in linked_list.h. Also sets
  * the next to NULL. If you want to add an element to 
@@ -23,11 +22,11 @@ LinkedList* create_list()
 }
 
 /**
- * @brief Creates a new list with newData as data and pushes it to
- * @brief the end of the list.
- * This functions allocates memory for a new element of the list and
- * pushes the data passed as parameter in this new element that is
- * then placed at the end of the list.
+ * Allocates memory for a new element of the list and
+ * pushes the data passed as parameter in this new element 
+ * that is then placed at the end of the list.
+ * @param list The list you wish to push_back to.
+ * @param newData The data to be pushed back.
  * @author Simon Scatton
  * @date 21/01/2020
  * @return true(1) if the push_back was successful false(0) otherwise
@@ -47,13 +46,12 @@ int push_back(LinkedList *list, int newData)
 }
 
 /**
- * @brief Removes the last element of the linked list and returns it.
- * This functions removes the last element of the linked list and 
- * returns it, it also sets the new end pointer
- * of the list to NULL. If the list passed as argument is NULL
- * or a sentinel, then the pop operation will fail and the pop
- * function will return NULL. You will need to free yourself the
- * struct after a call to pop
+ * Removes the last element of the linked list and returns it.
+ * It also sets the new end pointer of the list to NULL. 
+ * If the list passed as argument is NULL or a sentinel, then 
+ * the pop operation will fail and the pop function will return 
+ * NULL. You will need to free yourself the struct after a 
+ * call to pop.
  * @author Simon Scatton
  * @date 21/01/2020
  * @return A pointer to the end of the list if the list is not null
--- src/rationl.c
+++ src/rationl.c
@@ -1,10 +1,4 @@
-#include "automaton/automaton.h"
-
 int main()
 {
-    Automaton automaton = {
-        .state_count = 3,
-        .adj_list = NULL,
-    };
     return 0;
 }
