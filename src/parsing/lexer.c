6953f38b48455a085092d7d6a5a26d1977fd32cf
Rostan Tabet <rostan.tabet@gmail.com>
2021-03-05 00:13:04 +0100

    Implemented basic lexer with operators only
    Signed-off-by: Rostan Tabet <rostan.tabet@gmail.com>

--- src/parsing/lexer.c
+++ src/parsing/lexer.c
@@ -1,7 +1,28 @@
 #include "lexer.h"
+#include <stdio.h>
 
 Array *tokenize(char *string)
 {
     Array *tokens = Array(Token);
+
+    for (char c = *string; c != 0; c = *++string)
+    {
+        Token token;
+        token.value = c;
+        switch (c)
+        {
+        case '*':
+        case '|':
+        case '+':
+        case '?':
+            token.type = PUNCTUATION;
+            break;
+        default:
+            token.type = LITERAL;
+        }
+
+        array_append(tokens, &token);
+    }
+
     return tokens;
 }
