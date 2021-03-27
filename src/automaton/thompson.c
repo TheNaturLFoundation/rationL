259fc0a448f13ec9f7c64aa4aef75e82614ca063
Simon Scatton <simon.scatton@epita.fr>
2021-03-27 18:55:55 +0100

    Optimized concatenation memory usage
    Instead of creating a new automaton to concatenate the left
    and right automatons in a new automaton, the concatenate function
    now insert the right automaton in place in the left automaton so
    that it doesn't waste memory. concatenate is now void.

--- src/automaton/thompson.c
+++ src/automaton/thompson.c
@@ -14,68 +14,51 @@ int is_state_entry(Automaton *aut, State *state)
     return 0;
 }
 
-Automaton *concatenate(Automaton *a, Automaton *b)
+void concatenate(Automaton *a, Automaton *b)
 {
-    Automaton *result = Automaton();
+    Array *states_b_tab = Array(State *);
+    Array *terminal_states = Array(State *);
     arr_foreach(State *, state_a, a->states)
     {
-        State *state = State(0);
-        automaton_add_state(result, state, is_state_entry(a, state_a));
-    }
-    size_t index = 0;
-    arr_foreach(LinkedList *, transitions, a->adj_lists)
-    {
-        transitions = transitions->next;
-        while (transitions != NULL)
+        if (state_a->terminal)
         {
-            Transition *transition = transitions->data;
-            automaton_add_transition(
-                result, *(State **)array_get(result->states, index),
-                *(State **)array_get(result->states, transition->target->id),
-                transition->value, 0);
-            transitions = transitions->next;
+            array_append(terminal_states, &state_a);
+            state_a->terminal = 0;
         }
-
-        index += 1;
     }
-
-    Array *states_b_tab = Array(State *);
-
     arr_foreach(State *, state_b, b->states)
     {
         State *state = State(state_b->terminal);
-        state->id = result->size;
+        state->id = a->size;
         array_append(states_b_tab, &state);
-        automaton_add_state(result, state, 0);
+        automaton_add_state(a, state, 0);
     }
-    index = 0;
+    size_t index = 0;
     arr_foreach(LinkedList *, transition_b, b->adj_lists)
     {
         transition_b = transition_b->next;
         while (transition_b != NULL)
         {
             Transition *transition = transition_b->data;
-            State *src = *(State**)array_get(states_b_tab, index);
-            State *dst = *(State**)array_get(states_b_tab, transition->target->id);
-            automaton_add_transition(result, src, dst, transition->value,
+            State *src = *(State **)array_get(states_b_tab, index);
+            State *dst =
+                *(State **)array_get(states_b_tab, transition->target->id);
+            automaton_add_transition(a, src, dst, transition->value,
                                      transition->is_epsilon);
             index += 1;
             transition_b = transition_b->next;
         }
     }
-    arr_foreach(State*, a_states, a->states)
+    arr_foreach(State *, a_states, terminal_states)
     {
-        if(a_states->terminal)
+        arr_foreach(State *, entry_state, b->starting_states)
         {
-            arr_foreach(State*, entry_state, b->starting_states)
-            {
-                State* dst = *(State**)array_get(states_b_tab, entry_state->id);
-                automaton_add_transition(result, *(State**)array_get(result->states, a_states->id), dst, 'e', 1);
-            }
+            State *dst = *(State **)array_get(states_b_tab, entry_state->id);
+            automaton_add_transition(a, a_states, dst, 'e', 1);
         }
     }
+    array_free(terminal_states);
     array_free(states_b_tab);
-    return result;
 }
 
 Automaton *recur_thompson(BinTree *tree)
@@ -101,10 +84,9 @@ Automaton *recur_thompson(BinTree *tree)
     case CONCATENATION: {
         Automaton *left = recur_thompson(tree->left);
         Automaton *right = recur_thompson(tree->right);
-        Automaton *result = concatenate(left, right);
-        automaton_free(left);
+        concatenate(left, right);
         automaton_free(right);
-        return result;
+        return left;
     }
     default:
         break;
