537c166fd521109a91114c2f6949540d3671408a
Antoine Sicard <antoine.sicard@epita.fr>
2021-05-01 13:34:52 +0200

    minimization trash test

--- test/automaton/minimization_test.c
+++ test/automaton/minimization_test.c
@@ -6,14 +6,14 @@
 Test(minimization, tmp)
 {
     Automaton *a_end = automaton_from_daut(
-        TEST_PATH "automaton/determine_daut/a?b*a.daut", 3);
+        TEST_PATH "automaton/determine_daut/a_end.daut", 3);
 
     //automaton_to_dot(a_end);
 
     Automaton *minimized = minimize(a_end);
 
-    automaton_to_dot(minimized);
+    //automaton_to_dot(minimized);
 
     automaton_free(a_end);
-    //automaton_free(minimized);
+    automaton_free(minimized);
 }
