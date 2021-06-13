fa11c040e077fd80192c0e3316d6d1a150158ee9
vlad <vlad.argatu@epita.fr>
2021-06-13 18:04:53 +0200

    Added test for thp

--- test/automaton/thompson_test.c
+++ test/automaton/thompson_test.c
@@ -151,3 +151,55 @@ Test(thompson, identifier)
     array_free(arr);
     bintree_free(b);
 }
+
+Test(thompson, group_concat)
+{
+    Array *arr = tokenize("(b)c");
+    BinTree *b = parse_symbols(arr);
+    //bin_tree_to_dot(b, stdout);
+    Automaton *automaton = thompson(b);
+    //automaton_to_dot(automaton);
+    size_t g = 1;
+    
+    State * s[4];
+    for(int i = 0; i < 4; i++)
+    {
+        s[i] = *(State **)array_get(automaton->states, i);
+    }
+    Set * set;
+
+    cr_assert_eq(get_entering_groups(automaton, s[2], s[3], 'b', 0), NULL);
+    cr_assert_eq(get_entering_groups(automaton, s[3], s[0], 0, 1), NULL);
+    cr_assert_eq(get_entering_groups(automaton, s[0], s[1], 'c', 0), NULL);
+
+    set = get_entering_groups(automaton, NULL, s[2], 0, 1);
+    cr_assert_neq(set, NULL);
+    cr_assert_eq(set->size, 1);
+    cr_assert_neq(map_get(set, &g), NULL);
+
+    //leaving:
+    cr_assert_eq(get_leaving_group(automaton, NULL, s[2], 'b', 0), NULL);
+    cr_assert_eq(get_leaving_group(automaton, s[2], s[3], 'b', 0), NULL);
+    cr_assert_eq(get_leaving_group(automaton, s[0], s[1], 'c', 0), NULL);
+
+    set = get_leaving_group(automaton, s[3], s[0], 0, 1);
+    cr_assert_neq(set, NULL);
+    cr_assert_eq(set->size, 1);
+    cr_assert_neq(map_get(set, &g), NULL);
+
+    automaton_free(automaton);
+    array_free(arr);
+    bintree_free(b);
+}
+
+Test(thompson, simple)
+{
+    Array *arr = tokenize("(a)");
+    BinTree *b = parse_symbols(arr);
+    //tree_to_dot(b);
+    Automaton *aut = thompson(b);
+    automaton_to_dot(aut);
+    automaton_free(aut);
+    array_free(arr);
+    bintree_free(b);
+}
