6e61539f2361a29585b05f05fab9b6007a5fb332
Rostan Tabet <rostan.tabet@gmail.com>
2021-06-14 03:06:42 +0200

    It's a kind of magic
    Signed-off-by: Rostan Tabet <rostan.tabet@gmail.com>

--- src/automaton/determine.c
+++ src/automaton/determine.c
@@ -19,15 +19,30 @@ Automaton *determine(const Automaton *source)
     // Initialize the starting state
     int terminal = 0;
     Set *start_set = Set(size_t, &hash_size_t, &compare_size_t);
+    Set *entering_groups = Set(size_t, &hash_size_t, &compare_size_t);
     arr_foreach(State *, starting_state, source->starting_states)
     {
         if (starting_state->terminal)
             terminal = 1;
         set_add(start_set, &starting_state->id);
+
+        Set *groups = get_entering_groups((Automaton *)source, NULL,
+                                          starting_state, 0, 1);
+        if (groups != NULL)
+        {
+            map_foreach_key(size_t, group, groups,
+                            set_add(entering_groups, &group);)
+        }
     }
     // Push it in the queue
     State *first_state = State(terminal);
     automaton_add_state(automaton, first_state, 1);
+    {
+        map_foreach_key(size_t, group, entering_groups, {
+            automaton_mark_entering(automaton, NULL, first_state, 0, 0, group);
+        });
+    }
+    map_free(entering_groups);
     map_set(powersets, &start_set, &first_state->id);
     list_push_back(set_queue, &start_set);
 
@@ -41,7 +56,7 @@ Automaton *determine(const Automaton *source)
         map_clear(state_sets);
 
         // Initialize the `state_sets` dict
-        map_foreach_key(
+        { map_foreach_key(
             size_t, state_id, current_set,
             {
                 for (Letter c = 0; c < 255; c++)
@@ -63,45 +78,91 @@ Automaton *determine(const Automaton *source)
 
                     list_foreach(State *, next, list) set_add(set, &next->id);
                 }
-            })
+            }) }
 
-            // For each set of states, if it does not exist,
-            // create a corresponding state and map it.
-            // Create a transition from the current state to the next one.
-            map_foreach_key(Letter, value, state_sets, {
-                Set *set = *(Set **)map_get(state_sets, &value);
-                State *dst_state;
-                void *ptr = map_get(powersets, &set);
-                if (ptr == NULL)
-                {
-                    // If at least one dst_state is terminal in the source,
-                    // the new dst_state is terminal.
-                    terminal = 0;
-                    map_foreach_key(size_t, id, set, {
-                        State *state = *(State **)array_get(source->states, id);
-                        if (state->terminal)
-                        {
-                            terminal = 1;
-                            // map_foreach_key uses two loops, break isn't
-                            // enough
-                            goto out89;
-                        }
-                    }) out89:;
-                    dst_state = State(terminal);
-                    automaton_add_state(automaton, dst_state, 0);
-                    map_set(powersets, &set, &dst_state->id);
-                    list_push_back(set_queue, &set);
-                }
-                else
-                {
-                    map_free(set);
-                    size_t id = *(size_t *)ptr;
-                    dst_state = *(State **)array_get(automaton->states, id);
-                }
+        // For each set of states, if it does not exist,
+        // create a corresponding state and map it.
+        // Create a transition from the current state to the next one.
+        map_foreach_key(Letter, value, state_sets, {
+            Set *set = *(Set **)map_get(state_sets, &value);
+            State *dst_state;
+            void *ptr = map_get(powersets, &set);
+            int free_set = 0;
+            if (ptr == NULL)
+            {
+                // If at least one dst_state is terminal in the source,
+                // the new dst_state is terminal.
+                terminal = 0;
+                map_foreach_key(size_t, id, set, {
+                    State *state = *(State **)array_get(source->states, id);
+                    if (state->terminal)
+                    {
+                        terminal = 1;
+                        // map_foreach_key uses two loops, break isn't
+                        // enough
+                        goto out101;
+                    }
+                }) out101:;
+                dst_state = State(terminal);
+                automaton_add_state(automaton, dst_state, 0);
+                map_set(powersets, &set, &dst_state->id);
+                list_push_back(set_queue, &set);
+            }
+            else
+            {
+                free_set = 1; // Don't free it here: we need it later
+                size_t id = *(size_t *)ptr;
+                dst_state = *(State **)array_get(automaton->states, id);
+            }
 
-                automaton_add_transition(automaton, src_state, dst_state, value,
-                                         0);
-            })
+            automaton_add_transition(automaton, src_state, dst_state, value, 0);
+
+            map_foreach_key(size_t, src_id, current_set, {
+                State *src = *(State **)array_get(source->states, src_id);
+                map_foreach_key(size_t, dst_id, set, {
+                    State *dst = *(State **)array_get(source->states, dst_id);
+
+                    Set *groups = get_entering_groups((Automaton *)source, src,
+                                                      dst, value, 0);
+                    if (groups != NULL)
+                    {
+                        map_foreach_key(size_t, group, groups, {
+                            automaton_mark_entering(automaton, src_state,
+                                                    dst_state, value, 0, group);
+                        })
+                    }
+
+                    groups = get_leaving_group((Automaton *)source, src, dst,
+                                               value, 0);
+                    if (groups != NULL)
+                    {
+                        map_foreach_key(size_t, group, groups, {
+                            automaton_mark_leaving(automaton, src_state,
+                                                   dst_state, value, 0, group);
+                        })
+                    }
+                });
+            });
+
+            map_foreach_key(size_t, dst_id, set, {
+                State *dst = *(State **)array_get(source->states, dst_id);
+                if (dst->terminal)
+                {
+                    Set *groups =
+                        get_leaving_group((Automaton *)source, dst, NULL, 0, 0);
+                    if (groups != NULL)
+                    {
+                        map_foreach_key(size_t, group, groups, {
+                            automaton_mark_leaving(automaton, dst_state, NULL,
+                                                   value, 0, group);
+                        })
+                    }
+                }
+            });
+
+            if (free_set)
+                map_free(set);
+        })
     }
 
     list_free(set_queue);
--- test/automaton/utils.c
+++ test/automaton/utils.c
@@ -1,6 +1,9 @@
 #include "utils.h"
+
 #include <criterion/criterion.h>
 #include <criterion/internal/assert.h>
+
+static void assert_set_eq(size_t line, Set *s1, Set *s2);
 
 void assert_automaton_eq_(size_t line, Automaton *a1, Automaton *a2)
 {
@@ -14,13 +17,21 @@ void assert_automaton_eq_(size_t line, Automaton *a1, Automaton *a2)
         State *s1 = *(State **)array_get(a1->starting_states, i);
         int found = 0;
         arr_foreach(State *, s2, a2->starting_states)
-        if (s2->id == s1->id)
         {
-            found = 1;
-            break;
+            if (s2->id == s1->id)
+            {
+                found = 1;
+                break;
+            }
         }
-        cr_assert(found, "First automaton has state %zu as a starting state,"
-                         " second hasn't (line %zu)", s1->id, line);
+        cr_assert(found,
+                  "First automaton has state %zu as a starting state,"
+                  " second hasn't (line %zu)",
+                  s1->id, line);
+
+        s2 = *(State **)array_get(a2->starting_states, i);
+        assert_set_eq(line, get_entering_groups(a1, NULL, s1, 0, 0),
+                      get_entering_groups(a2, NULL, s2, 0, 0));
     }
     cr_assert_eq(a1->states->size, a2->states->size,
                  "First automaton has %zu states, second has %zu (line %zu)",
@@ -28,10 +39,8 @@ void assert_automaton_eq_(size_t line, Automaton *a1, Automaton *a2)
 
     size_t terminal1 = 0;
     size_t terminal2 = 0;
-    arr_foreach(State *, s1, a1->states)
-    terminal1 += s1->terminal;
-    arr_foreach(State *, s2, a2->states)
-    terminal2 += s2->terminal;
+    arr_foreach(State *, s1, a1->states) terminal1 += s1->terminal;
+    arr_foreach(State *, s2, a2->states) terminal2 += s2->terminal;
     cr_assert_eq(terminal1, terminal2,
                  "First automaton has %zu terminal states, "
                  "second has %zu (line %zu)",
@@ -42,14 +51,16 @@ void assert_automaton_eq_(size_t line, Automaton *a1, Automaton *a2)
         if (!s1->terminal)
             continue;
         int found = 0;
-        arr_foreach(State *, s2, a2->states)
-        if (s2->terminal && s2->id == s1->id)
+        arr_foreach(State *, s2,
+                    a2->states) if (s2->terminal && s2->id == s1->id)
         {
             found = 1;
             break;
         }
-        cr_assert(found, "First automaton has state %zu as a terminal state,"
-                         " second hasn't (line %zu)", s1->id, line);
+        cr_assert(found,
+                  "First automaton has state %zu as a terminal state,"
+                  " second hasn't (line %zu)",
+                  s1->id, line);
     }
 
     cr_assert_eq(a1->size, a2->size,
@@ -64,11 +75,14 @@ void assert_automaton_eq_(size_t line, Automaton *a1, Automaton *a2)
     for (size_t c = 0; c < NUMBER_OF_SYMB; c++)
         for (size_t src = 0; src < a1->size; src++)
         {
-            LinkedList *list1 = get_matrix_elt(a1, src, c, c == EPSILON_INDEX);
-            LinkedList *list2 = get_matrix_elt(a2, src, c, c == EPSILON_INDEX);
+            int is_epsilon = c == EPSILON_INDEX;
+            State *s1 = *(State **)array_get(a1->states, src);
+            State *s2 = *(State **)array_get(a2->states, src);
+            LinkedList *list1 = get_matrix_elt(a1, src, c, is_epsilon);
+            LinkedList *list2 = get_matrix_elt(a2, src, c, is_epsilon);
             if (list1 == NULL || list2 == NULL)
-                cr_assert_eq(list1, list2, "%p != %p (line: %zu)",
-                             list1, list2, line);
+                cr_assert_eq(list1, list2, "%p != %p (line: %zu)", list1, list2,
+                             line);
             else
             {
                 list1 = list1->next;
@@ -76,14 +90,66 @@ void assert_automaton_eq_(size_t line, Automaton *a1, Automaton *a2)
             }
             while (list1 != NULL && list2 != NULL)
             {
-                State *s1 = *(State **)list1->data;
-                State *s2 = *(State **)list2->data;
-                cr_assert_eq(s1->id, s2->id);
-                cr_assert_eq(s1->terminal, s2->terminal);
+                State *d1 = *(State **)list1->data;
+                State *d2 = *(State **)list2->data;
+                cr_assert_eq(d1->id, d2->id);
+                cr_assert_eq(d1->terminal, d2->terminal);
+
+                assert_set_eq(line, get_entering_groups(a1, s1, d1, c, is_epsilon),
+                              get_entering_groups(a2, s2, d2, c, is_epsilon));
+                assert_set_eq(line, get_leaving_group(a1, s1, d1, c, is_epsilon),
+                              get_leaving_group(a2, s2, d2, c, is_epsilon));
 
                 list1 = list1->next;
                 list2 = list2->next;
             }
             cr_assert_eq(list1, list2);
+
+            if (s1->terminal)
+            {
+                assert_set_eq(line, get_leaving_group(a1, s1, NULL, c, is_epsilon),
+                              get_leaving_group(a2, s2, NULL, c, is_epsilon));
+            }
         }
+}
+
+static void assert_set_eq(size_t line, Set *s1, Set *s2)
+{
+    if (s1 == NULL || s2 == NULL)
+    {
+        cr_assert_eq(s1, s2, "s1 == %p, s2 == %p (line %zu)", s1, s2, line);
+        return;
+    }
+    if (s1->buckets == NULL || s2->buckets == NULL)
+    {
+        cr_assert_eq(s1->buckets, s2->buckets,
+                     "s1->buckets == %p, s2->buckets == %p (line %zu)", s1, s2,
+                     line);
+        return;
+    }
+    cr_assert_eq(
+        s1->buckets->size, s2->buckets->size,
+        "first bucket has size %zu, second bucket has size %zu (line %zu)",
+        s1->buckets->size, s2->buckets->size, line);
+
+    for (size_t i = 0; i < s1->buckets->size; i++)
+    {
+        LinkedList *bucket1 = *(LinkedList **)array_get(s1->buckets, i);
+        LinkedList *bucket2 = *(LinkedList **)array_get(s2->buckets, i);
+        while (bucket1->next != NULL && bucket2->next != NULL)
+        {
+            MapNode *node1 = *(MapNode **)bucket1->next->data;
+            MapNode *node2 = *(MapNode **)bucket2->next->data;
+
+            size_t *x1 = node1->key;
+            size_t *x2 = node2->key;
+            cr_assert_eq(*x1, *x2, "%zu != %zu (line %zu)\n", *x1, *x2, line);
+
+            bucket1 = bucket1->next;
+            bucket2 = bucket2->next;
+        }
+        cr_assert_eq(bucket1->next, bucket2->next,
+                     "First bucket is greater than the second (line %zu)\n",
+                     line);
+    }
 }
